e82867e5fa9f456d58273a743b384e79fa8e74dc
icocampos <icarocampos97@gmail.com>
2021-11-25 22:51:46 -0300

    add button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,12 +4,15 @@ import { View } from 'react-native';
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
 import { styles } from './styles';
+import { SignInBox } from '../../components/SignInBox';
+
 
  export function Home() {
     return (
         <View style={styles.container}>  
             <Header />
             <MessageList />
+            <SignInBox />
         </View>
     );
 }
